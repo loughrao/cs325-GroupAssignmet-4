19fbc99d7b862859bf8c574aabae779d84e74c88
Garrett Howe <howega@kec1130-24.engr.oregonstate.edu>
2017-11-27 21:36:57 -0800

    File I/O

--- lights.py
+++ lights.py
@@ -3,8 +3,12 @@ inp = open("input.txt", "r");
 i = 0;
 j = 0;
 
-things = [[int(n) for n in line.rstrip("\n").split(",")]for line in inp];	
-X = things[0][0];
-array = [[0]*X]*X;
-for i in range(X):
-	array[i] = things[i+1]
+array = [[int(n) for n in line.rstrip("\n").split(",")]for line in inp];	
+n = array[0][0];
+m = array[0][1];
+lights = [0]*m
+switches =[[0]*m]*n
+for i in range(m):
+	lights[i] = array[1][i]
+for i in range(n):
+	switches[i] = array[i+2]
